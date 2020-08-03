57f6a0f01d89ae477f65b1548f42bb4fae46d3c9
Ujjawl <srivastavujjawal123@gmail.com>
2020-08-03 17:13:16 +0530

    'deploy'

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+if (process.env.TTPE === 'PROD') {
+    app.use(express.static('frontend/build'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+  }
+
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening at http://localhost:${process.env.PORT}`)
 })
