ea711bb54b67c462897f8d88581dbcaf7bf9c43e
Ujjawl <srivastavujjawal123@gmail.com>
2020-08-06 21:51:30 +0530

    'deploy'

--- frontend/src/actions/admin.js
+++ frontend/src/actions/admin.js
@@ -15,28 +15,31 @@ export const deleteid=(id,token)=>async dispatch=>{
     axios.post("/api/del",{token:token,id:id})
     .then((res)=>{
         alert(res.data.msg)
+        dispatch(getlist())
     })
-    dispatch(getlist())
+    
 }
 
 export const addnew=(token)=>async dispatch=>{
     axios.post("/api/add",{token:token,name:prompt("Provide name of new hacker")})
     .then((res)=>{
         alert(res.data.msg)
+        dispatch(getlist())
     })
     .catch(()=>{
         alert("Addition Failed Please Sanitize the value")
     })
-    dispatch(getlist())
+    
 }
 
 export const update=(token,id,data)=>async dispatch=>{
     axios.post("/api/update",{token:token,id:id,update:data})
     .then((res)=>{
         alert(res.data.msg)
+        dispatch(getlist())
     })
     .catch(()=>{
         alert("Updation Failed Please Sanitize the value or Acces denied")
     })
-    dispatch(getlist())
+    
 }
